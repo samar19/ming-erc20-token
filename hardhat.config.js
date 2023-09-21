9043f9dfe8689a3315a7938be0e3feac129a9f17
Ming-der Wang <mingderwang@gmail.com>
2023-09-21 22:47:15 +0800

    ok for npx hardhat run scripts/deploy_upgradeable_mingtoken.js --network mumbai

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
+require('@openzeppelin/hardhat-upgrades');
 
 require("dotenv").config();
 
--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -4,7 +4,8 @@ const { ethers, upgrades } = require("hardhat");
 async function main() {
   const [deployer] = await ethers.getSigners();
 
-  const proxyAddress = "0x74De8cA31af474CEd4E6789Ae525cd7bc9ce5Ec4"; // Replace with the actual address
+  // Replace with the actual address you deployed contract address.
+  const proxyAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; 
 
   const MINGTokenV2 = await ethers.getContractFactory("MINGTokenV2");
   const proxyContract = await upgrades.upgradeProxy(proxyAddress, MINGTokenV2);
