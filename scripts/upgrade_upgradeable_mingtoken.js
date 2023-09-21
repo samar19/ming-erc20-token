8b1e233f4dfe31a08778a29838e8745e80546466
Ming-der Wang <mingderwang@gmail.com>
2023-09-21 23:05:20 +0800

    modify upgrade script, but not tested yet

--- scripts/upgrade_upgradeable_mingtoken.js
+++ scripts/upgrade_upgradeable_mingtoken.js
@@ -1,10 +1,12 @@
 // scripts/upgrade-box.js
 const { ethers, upgrades } = require("hardhat");
+const BOX_ADDRESS='0xff37cf4abf6e756e35c24e466b93fd6028866157'
+// above address have to be the current contract address 
 
 async function main() {
-  const BoxV2 = await ethers.getContractFactory("BoxV2");
+  const BoxV2 = await ethers.getContractFactory("MINGTokenV2");
   const box = await upgrades.upgradeProxy(BOX_ADDRESS, BoxV2);
-  console.log("Box upgraded");
+  console.log("MINGTokenV2 is upgraded");
 }
 
 main();
